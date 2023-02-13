d96f216bd220e5b5b6a123ce337574a61495e4f7
QuickMash <106212829+QuickMash@users.noreply.github.com>
2023-02-13 11:49:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,6 @@ console.warn("DO NOT use any hacky meathods to start the server!");
 console.info("------------------------------")
 console.info("BTW QuickMash is cool")
 }
+function startserver() {
+      alert("MineHut Currently does not support webhooks./nsorry for any problems./n use /join QuickStealLS to start server.");
+}
