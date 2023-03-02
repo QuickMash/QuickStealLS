a93e25e8f19cb9c699d0ff8e614bd2278895f476
QuickMash <106212829+QuickMash@users.noreply.github.com>
2023-03-02 14:47:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-function load()
-console.info("Website Loading!")
-console.info("------------------------------")
-console.warn("This terminal is for debug use only!");
-console.warn("DO NOT use any hacky meathods to start the server!");
-console.info("------------------------------")
-console.info("BTW QuickMash is cool")
-}
-function startserver() {
-      alert("MineHut Currently does not support webhooks./nsorry for any problems./n use /join QuickStealLS to start server.");
-}
+console.warn("This terminal is for website staff and developers");
+console.log("QuickStealLS MC server IP:");
+console.log("QuickstealLS.minehut.gg");
