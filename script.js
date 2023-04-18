13900cacb98fdb6c51fbeee235d594c63ac5e46d
QuickMash <106212829+QuickMash@users.noreply.github.com>
2023-04-18 11:56:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-console.warn("This terminal is for website staff and developers");
-console.log("QuickStealLS MC server IP:");
-console.log("QuickstealLS.minehut.gg");
+var css = "text-shadow: 1px 1px 2px black, 0 0 1em red, 0 0 0.2em darkred; font-size: 40px;"
+console.log("%cSTOP!", css);
+console.log("This terminal is for website staff and developers");
+console.error("ur hot uwu");
