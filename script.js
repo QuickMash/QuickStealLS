c90f1d86eaef18e6f25ff4e0e9bc1d6de4243a1d
QuickMash <106212829+QuickMash@users.noreply.github.com>
2023-04-20 14:47:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-var css = "text-shadow: 1px 1px 2px black, 0 0 1em red, 0 0 0.2em darkred; font-size: 40px;"
-console.log("%cSTOP!", css);
-console.log("This terminal is for website staff and developers");
-console.error("ur hot uwu");
+function mouse() {
+  docume
+}
