e3a23288000ed2daee18787ac928264afc79c855
QuickMash <106212829+QuickMash@users.noreply.github.com>
2023-05-02 15:08:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function mouse() {
-  var mode = document.getElementById("myCheck");
-  if (myCheck = true) {
- alert("It Worked");
-}
-}
+let fakeAd = document.createElement("div");
+fakeAd.className =
+"textads banner-ads banner_ads ad-unit ad-zone ad-space adsbox"
+	
+fakeAd.style.height = "1px"
+	
+document.body.appendChild(fakeAd)
+	
+let x_width = fakeAd.offsetHeight;
+let msg = document.getElementById("msg")
+	
+	
+	if(x_width){
+	console.log("No AdBlocker Detected")
+	}else{
+	alert("Please Disable your adblocker");
+	}
